9196f15a2513896b4e0c9eae18aafa035f7678e7
David Gasaway <dave@gasaway.org>
2022-02-27 09:44:26 -0800

    Apply new channel layout fix to 'toopus'.

--- towebm/toopus.py
+++ towebm/toopus.py
@@ -37,10 +37,11 @@ def main():
         action=DelimitedValueAction, dest='audio_quality', metavar='BITRATE', value_type=int,
         default=[160])
     parser.add_argument('--channel-layout-fix',
-        help='1 to apply a 5.1 channel layout fix (necessary for 5.1 with side channels), or 0 to '
-        'apply no fix; may be a colon-delimited list to select from multiple source audio tracks, '
-        'with value 0 or blank used to skip a track',
-        action=DelimitedValueAction, metavar='FIX_STRING', value_type=int, default=[0])
+        help='apply a channel layout fix to 4.1, 5.0, 5.1(side) audio sources to output a '
+             'compatible 5.1(rear) layout; may be a colon-delimited list to apply the fix to '
+             'multiple audio tracks from the source; use 0 or blank to apply no fix',
+        action=DelimitedValueAction, metavar="FIX_STRING",
+        value_choices=['0', '4.1', '5.0', '5.1'], default=['0'])
 
     # Timecode/segment arguments.
     add_timecode_arguments(parser)
