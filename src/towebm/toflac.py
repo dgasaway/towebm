1270a764a4981776c672bfa318ce74f3e9a4c648
David Gasaway <dave@gasaway.org>
2025-02-19 23:40:52 -0800

    Add toflac.

---
+++ src/towebm/toflac.py
@@ -0,0 +1,26 @@
+# toflac - Converts audio/video files to audio-only opus using ffmpeg.
+# Copyright (C) 2025 David Gasaway
+# https://github.com/dgasaway/towebm
+
+# This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program; if not,
+# see <http://www.gnu.org/licenses>.
+import sys
+
+from towebm.converters import AudioConverter
+from towebm.formats import AudioFormats
+
+# --------------------------------------------------------------------------------------------------
+def main() -> int:
+    return AudioConverter(AudioFormats.FLAC).main()
+
+# --------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+    sys.exit(main())
